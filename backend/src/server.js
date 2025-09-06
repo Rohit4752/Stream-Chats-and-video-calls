469db78da6dffa3561c89f20f4d12b547eac44f7
Rohit <rohitisl2020@gmail.com>
2025-09-06 18:29:34 +0530

    main

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.APP_URL, // allow frontend to access
     credentials: true, // allow frontend to send cookies
   })
 );
